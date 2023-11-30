b64c82f3cfcde4140ab76f637325d8b175363197
TEMAS629 <94691533+TEMAS629@users.noreply.github.com>
2023-11-30 13:08:33 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,58 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+
+    loop: true,
+
+    autoplay: {
+        delay: 5000
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('element-show');
+        }
+    });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+for (let elm of elements) {
+    observer.observe(elm);
+}
+
+$(document).ready(function () {
+    // = Вешаем событие прокрутки к нужному месту
+    //	 на все ссылки якорь которых начинается на #
+    $('a[href^="#"]').bind('click.smoothscroll', function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+            $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
+});
